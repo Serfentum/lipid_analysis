5e62fd9f94673c4044fc8153beee194c7800a9ae
arleg <tetpapx@bk.ru>
2018-09-04 18:19:56 +0300

    Add z-scaling to scalings, incorporate it into overall scaling

--- functions/processing_dataset/scaling.py
+++ functions/processing_dataset/scaling.py
@@ -1,16 +1,57 @@
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from .column_division import *
 
 
-def log_scaling(df):
+def z_scale(df):
     """
-    Log scaling of (intensities + 1) in df. Addition of 1 to avoid Inf in case of zeros.
+    Perform z-scaling on df
+    samples constant should be predefined
     :param df: df - dataframe
-    :return: df - scaled dataframe
+    :return:
     """
-    dff = df.copy()
+    df = df.copy()
 
-    # Take log(dff + 1) from intensities
-    dff[samples] = np.log1p(dff[samples])
-    return dff
+    # Initialize scaler and scale numeric data
+    ss = StandardScaler()
+    df[samples] = ss.fit_transform(df[samples])
+    return df
+
+
+def log_transform(df):
+    """
+    Log scaling of (intensities + minimum) in df. Addition of minimum to avoid Inf in case of zeros.
+    Minimum is a half of minimal positive value
+    samples constant should be predefined
+    :param df: df - dataframe with nonnegative values
+    :return: df - log transformed dataframe
+    """
+    df = df.copy()
+
+    # Adding 1 before log transformation is too much for z-scaled positivised data
+    # Thus I have apply this general method:
+    # According to this post, one of appropriate adding to 0 before log-transforming is half of nonnegative minimum
+    # value
+    # https://robjhyndman.com/hyndsight/transformations/
+    # Find positive minimum to add to values
+    minimum = df[samples][df[samples] != 0].min().min()
+
+    # Take  from intensities
+    df[samples] = np.log(df[samples] + minimum)
+    return df
+
+
+def positivize(df):
+    """
+    Add minimal value in df to df (shift value distribution to start from 0)
+    samples constant should be predefined
+    :param df: df - dataframe
+    :return: df - nonnegative (0 and positives) dataframe
+    """
+    df = df.copy()
+
+    # Find minimum and subtract it from df
+    minimum = df[samples].min().min()
+    df[samples] = df[samples] - minimum
+    return df
